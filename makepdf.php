82df6fbd40a08d128df50ef5e39b1306780b7dda
reda-coder <redaelouahabi98@gmail.com>
2021-03-27 20:47:08 +0100

    Note of PhpMailer

--- makepdf.php
+++ makepdf.php
@@ -147,6 +147,54 @@ $data .="<h2 style='font-weight: 300;'> Totale =_________ $prixTot dh</h2>";
 $mpdf->WriteHTML($data);
 //output to browser  "hello.pdf",'D'
 $filename="BabySafetyBonDeCommande".date('Y-m-d H-i-s').".pdf";
-$mpdf -> Output($filename,"D");
+$mpdf -> Output($filename,"D"); //D for download
 // $pdf =$mpdf -> Output('','S');
+
+/**
+ * to be continued
+ */
+
+
+// I was Working on sending email by phpMailer , but I stopped it because 
+// I fing some problemes on how to configure the composer by adding phpmailer liberary (on the composer)
+// but here is the lines of code that we will put instead of the Download Output
+
+// //run fct
+// SendEmail($pdf,$enqueryData);
+
+// function SendEmail($pdf,$enqueryData){
+
+// //Instantiation and passing `true` enables exceptions
+// $mail = new PHPMailer(true);
+
+// try {
+//     //Server settings
+//     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+//     $mail->isSMTP();                                            //Send using SMTP
+//     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+//     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+//     $mail->Username   = 'emi.shop.pi@gmail.com';                     //SMTP username
+//     $mail->Password   = 'emi.shop.piemi.shop.pi';                               //SMTP password
+//     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+//     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+//     //Recipients
+//     $mail->setFrom('redaelouahabi@student.emi.ac.ma', 'Mailer');
+//     $mail->addAddress('redaelouahabi98@gmail.com', 'Joe User');     //Add a recipient
+    
+
+
+
+//     //Content
+//     $mail->isHTML(true);                                  //Set email format to HTML
+//     $mail->Subject = 'Here is the subject';
+//     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+//     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+//     $mail->send();
+//     echo 'Message has been sent';
+// } catch (Exception $e) {
+//     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+// }
+// }
 ?>
